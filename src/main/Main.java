1d48c2822f031c701d94482411df801ac3df85dd
alexEstudo <alex-handsome@hotmail.com>
2020-06-16 14:08:38 -0300

    Meu primeiro commit dentro do netBeans.

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Scanner;
+
+
+public class Main{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n;
+        int a = input.nextInt();
+        
+        final int x = a;
+        
+        do{
+            n = input.nextInt();
+        }while(n <= 0);
+
+        for(int c = 1; c < n; c++){
+            a+= x + c;
+        }
+        
+        System.out.println(a);
+    }
+}
